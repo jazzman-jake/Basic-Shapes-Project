995b344c2a9985561368a5871dc779c354976551
jazzman-jake <williamsonj0403@gmail.com>
2025-05-11 13:22:44 -0400

    Create variety of classes, each for using different formulas and variables for that specific shape. Arrange together by including all classes in main.cpp to execute.

--- Basic Shapes Project.cpp
+++ Basic Shapes Project.cpp
@@ -5,7 +5,7 @@
 #include "Square.h"
 
 int main() {
-    std::vector<BasicShape*> shapes;
+    vector<BasicShape*> shapes;
 
     shapes.push_back(new Circle(0, 0, 5, "Small Circle"));
     shapes.push_back(new Circle(3, 4, 10, "Large Circle"));
@@ -13,9 +13,9 @@ int main() {
     shapes.push_back(new Rectangle(2, 10, "Tall Rectangle"));
     shapes.push_back(new Square(7, "Perfect Square"));
 
-    std::cout << "Shape Summary:\n";
+    cout << "Shape Summary:\n";
     for (BasicShape* shape : shapes) {
-        std::cout << shape->getName() << " - Area: " << shape->getArea() << '\n';
+        cout << shape->getName() << " - Area: " << shape->getArea() << '\n';
     }
 
     for (BasicShape* shape : shapes)
